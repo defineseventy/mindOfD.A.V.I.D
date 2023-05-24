d6efb09fd9c0677d5d9e2da02081e3082fe12673
P1935404 <63201020+P1935404@users.noreply.github.com>
2023-05-25 00:25:58 +0800

    in a mess

--- FED/index.js
+++ FED/index.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('FED'))
 
 app.get("/", (req, res) => {
-    res.sendFile("/public/index.html", { root: __dirname });
+    res.sendFile("/index.html", { root: __dirname });
 });
 
-const PORT = 3001; //We cannot use port 3000 because it has been occupied by our backend.
+app.get("/index.css", (req, res) => {
+    res.sendFile("/index.css", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile("/about.html", { root: __dirname });
+});
+
+app.get("/instructions", (req, res) => {
+    res.sendFile("/instructions.html", { root: __dirname });
+});
+
+app.get("/selection", (req, res) => {
+    res.sendFile("/selection.html", { root: __dirname });
+});
+
+app.get("/img/home/about.png", (req, res) => {
+    res.sendFile("/img/home/about.png", { root: __dirname });
+});
+
+app.get("/img/home/instructions.png", (req, res) => {
+    res.sendFile("/img/home/instructions.png", { root: __dirname });
+});
+
+app.get("/img/home/selection.png", (req, res) => {
+    res.sendFile("/img/home/selection.png", { root: __dirname });
+});
+
+
+const PORT = 3002; //We cannot use port 3000 because it has been occupied by our backend.
 app.listen(PORT, () => {
     console.log(`Client server has started listening on port ${PORT}`);
 });
